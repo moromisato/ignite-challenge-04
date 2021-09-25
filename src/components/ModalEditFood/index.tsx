21200af80ba0092c6511da6506362b766c4e178c
Luis Moromisato <moromisato.luis@gmail.com>
2021-09-25 14:17:08 -0300

    changed modal edit food to use tsx

---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,48 @@
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import Modal from '../Modal';
+import { Input } from '../Input';
+import { FormHandles } from '@unform/core';
+import { Food } from '../Food';
+import { createRef } from 'react';
+
+interface ModalEditFoodProps {
+  isOpen: boolean,
+  setIsOpen: () => void,
+  handleUpdateFood: (food: Food) => void,
+  editingFood: Food
+}
+
+function ModalEditFood({isOpen, editingFood, setIsOpen, handleUpdateFood}: ModalEditFoodProps) {
+  const formRef = createRef<FormHandles>();
+
+  const handleSubmit = async (data: Food) => {
+    handleUpdateFood(data);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+
+}
+
+export default ModalEditFood;
