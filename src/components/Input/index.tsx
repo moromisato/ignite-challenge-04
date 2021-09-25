96d5dc0c7b4ed8ffc0d24b74893846dd83f2cc44
Luis Moromisato <moromisato.luis@gmail.com>
2021-09-25 13:54:03 -0300

    converted input component to tsx

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -6,11 +6,15 @@ import {
 } from 'react';
 
 import { useField } from '@unform/core';
-
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string,
+  icon: any
+}
+
+export const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -49,5 +53,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
